8b4b62fcce5c619ca6ab679cd523efb8f436c386
Phil Seaton <phil@phil-seaton.com>
2014-09-06 00:58:01 -0700

    Added a bunch of Geo.Curve code, and a substantially failing test to go with it. Everything looks right, but the new curve's pointer is always 0

--- tests/ghop1/spec/SISL_constructors.js
+++ tests/ghop1/spec/SISL_constructors.js
@@ -1,38 +1,55 @@
 define(["SISL"],function(Geo){
     return [
-        "Wrapper Functions:",
+        "Wrapper Functions -->",
         function(){
-            it("Geo.Point Throws an error if passed an array of numbers",function(){
-                expect(function(){new Geo.Point([3.1415,100000,-200])}).toThrowError("Must pass 3 numbers to create a Point");
+            describe("GeoPoint",function(){
+                it("Throws an error if passed an array of numbers",function(){
+                    expect(function(){new Geo.Point([3.1415,100000,-200])}).toThrowError("Must pass 3 numbers to create a Point");
+                });
+                it("Throws an error if passed no parameters",function(){
+                    expect(function(){new Geo.Point()}).toThrowError("Must pass 3 numbers to create a Point");
+                });
+                it("Constructor Accepts 3 numbers, and stores a _pointer property",function(){
+                    var point = new Geo.Point(3.1415,100000,-200);
+                    expect(point._pointer).toBeDefined();
+                    expect(typeof point._pointer).toEqual("number");
+                });
+                it("Returns its pointer using 'getPointer",function(){
+                    var point = new Geo.Point(3.1415,100000,-200);
+                    expect(point._pointer).toBeDefined();
+                    expect(typeof point._pointer).toEqual("number");
+                    expect(point._pointer).toEqual(point.getPointer());
+                });
+                it("Returns its coordinates using 'getCoordsArray'",function(){
+                    var point = new Geo.Point(3.1415,100000,-200);
+                    var coordsArray = point.getCoordsArray();
+                    expect(coordsArray[0]).toBeCloseTo(3.1415);
+                    expect(coordsArray[1]).toEqual(100000);
+                    expect(coordsArray[2]).toEqual(-200);
+                });
+                it("Returns a float32Array object using 'getCoords'",function(){
+                    var point = new Geo.Point(3.1415,100000,-200);
+                    var float32arr = point.getCoords();
+                    expect(float32arr).toBeDefined();
+                    expect(typeof float32arr).toEqual("object");
+                    expect(typeof float32arr.byteLength).toEqual("number");
+                    expect(float32arr.byteLength).toEqual(3 * Float32Array.BYTES_PER_ELEMENT);
+                });
             });
-            it("Geo.Point Throws an error if passed no parameters",function(){
-                expect(function(){new Geo.Point()}).toThrowError("Must pass 3 numbers to create a Point");
-            });
-            it("Geo.Point Constructor Accepts 3 numbers, and stores a _pointer property",function(){
-                var point = new Geo.Point(3.1415,100000,-200);
-                expect(point._pointer).toBeDefined();
-                expect(typeof point._pointer).toEqual("number");
-            });
-            it("Geo.Point Returns its pointer using 'getPointer",function(){
-                var point = new Geo.Point(3.1415,100000,-200);
-                expect(point._pointer).toBeDefined();
-                expect(typeof point._pointer).toEqual("number");
-                expect(point._pointer).toEqual(point.getPointer());
-            });
-            it("Geo.Point Returns its coordinates using 'getCoordsArray'",function(){
-                var point = new Geo.Point(3.1415,100000,-200);
-                var coordsArray = point.getCoordsArray();
-                expect(coordsArray[0]).toBeCloseTo(3.1415);
-                expect(coordsArray[1]).toEqual(100000);
-                expect(coordsArray[2]).toEqual(-200);
-            });
-            it("Geo.Point Returns a float32Array object using 'getCoords'",function(){
-                var point = new Geo.Point(3.1415,100000,-200);
-                var float32arr = point.getCoords();
-                expect(float32arr).toBeDefined();
-                expect(typeof float32arr).toEqual("object");
-                expect(typeof float32arr.byteLength).toEqual("number");
-                expect(float32arr.byteLength).toEqual(3 * Float32Array.BYTES_PER_ELEMENT);
+            describe("GeoCurve",function(){
+                it("Throws error when passed generic objects for coordinates");
+                it("Throws error when passed a single GeoPoint not inside an array");
+                it("Throws error when passed a non-integer curve degree");
+                it("Throws error when passed a string or number for periodicity ");
+                it("Returns a pointer using 'getPointer' after being passed proper parameters",function(){
+                    var start = new Geo.Point(0,0,0),
+                        end = new Geo.Point(1,1,0),
+                        curve = new Geo.Curve([start,end],1,false);
+
+                    expect(curve.getPointer()).not.toEqual(0);
+                    expect(typeof curve.getPointer()).toEqual("number");
+                });
+                it("Returns getLength() ~~ sqrt(2) when passed (0,0) and (1,1), degree 1, non-periodic")
             });
         }
     ]
--- tests/ghop1/src/SISL/js/constructors.js
+++ tests/ghop1/src/SISL/js/constructors.js
@@ -1,6 +1,8 @@
 define(["src/SISL/js/sisl","underscore"],function(){
-    if (_.isUndefined(Module._newPoint)) console.warn("SISL routine newPoint is missing, but PointComponent depends on it.");
     var newPoint = Module.cwrap('newPoint','number',['number','number','number']);
+    var pointCoords = Module.cwrap('pointCoords','number',['number']);
+    var newCurve = Module.cwrap('newCurve','number',['number','number','number','number','number','number','number']);
+
     var Geo = {};
 
     Geo.Point = function GeoPoint(x, y, z){
@@ -17,9 +19,7 @@ define(["src/SISL/js/sisl","underscore"],function(){
             return this._pointer;
         },
         getCoords: function(){
-            //console.log('point pointer',this._pointer);
-            var coordsPtr = Module.ccall('pointCoords','number',['number'],[this._pointer]);
-            //console.log('coords pointer',coordsPtr);
+            var coordsPtr = pointCoords(this._pointer);
             return new Float32Array(Module.HEAPU8.buffer, coordsPtr, 3);
         },
         getCoordsArray: function(){
@@ -33,7 +33,72 @@ define(["src/SISL/js/sisl","underscore"],function(){
     // SISLCurve *newCurve (vertex_count, curve_order, *knotvector, *vertices, ikind, dimension, icopy)
     // ikind: 1=polynomial b-spline, 2=rational b-spline, 3=polynomial bezier, 4=rational bezier
     // icopy: 0=Set pointer to input arrays, 1=Copy input arrays, 2=Set pointer and remember to free arrays.
-    Geo.Curve = function GeoCurve(controlPoints, degree, closed){};
+    Geo.Curve = function GeoCurve(controlPoints, degree, periodic){
+        // validate inputs
+        var controlPointsPass = true;
+        if (!_.isArray(controlPoints) || controlPoints.length < 2) {
+            controlPointsPass = false;
+        } else {
+            _.each(controlPoints, function(pt){
+                if (pt.constructor.name !== "GeoPoint"){
+                    controlPointsPass = false;
+                }
+            });
+        }
+        if (!controlPointsPass) {throw new Error("Controlpoints must be an array of at least 2 Geo.Point objects");}
+        if (typeof degree !== "number" || degree % 1 !== 0) {throw new Error("Curve degree must be an integer");}
+        if (typeof periodic !== "boolean") {throw new Error("Periodic must be a boolean");}
+
+        // By definition, from http://en.wikipedia.org/wiki/Non-uniform_rational_B-spline
+        // #knots = #control pts + curve order
+        // curve order = degree + 1
+        // knot vectors look like [0,0,1,2,3,3]
+
+        // Preparing data for geometry library:
+        var vertexCount = controlPoints.length,
+            curveOrder = degree + 1,
+            ikind = 3, // polynomial bezier by default?
+            dimension = 3, // always
+            icopy = 0,
+            knotVector = [], knotVectorPointer,
+            vertices = [], verticesPointer;
+
+        console.warn("NEED A C FUNCTION THAT CAN TAKE LOTS OF ARRAY POINTERS AND PUT THOSE VALUES INTO A NEW SINGLE ARRAY OF VERTICES");
+        // Pile control point values into a single array, push that to C heap. THIS SHOULD BE A C FUNCTION!
+        _.each(controlPoints,function(pt){
+            vertices = vertices.concat(pt.getCoordsArray());
+        });
+        verticesPointer = Module.Utils.copyJSArrayToC(vertices);
+
+        // Pile up a generic knot vector. There are 2 zeros to start, and two of the last digit.
+        // There should be even spacing between all numbers so that all control points have equal weight.
+        knotVector.push(0);
+        knotVector.push(0);
+        var i = 1;
+        // ... loop
+        i++;
+        knotVector.push(i);
+        knotVector.push(i);
+
+        // copy knotvector, get pointer:
+        knotVectorPointer = Module.Utils.copyJSArrayToC(knotVector);
+        console.log('knot vect: ',knotVector,knotVectorPointer);
+        console.log('vertex vect: ',vertices,verticesPointer);
+console.log(vertexCount,curveOrder,knotVectorPointer,verticesPointer,ikind,dimension,icopy);
+        this._pointer = newCurve(vertexCount,curveOrder,knotVectorPointer,verticesPointer,ikind,dimension,icopy);
+        console.log('HARVESTED POINTER: ',this._pointer);
+    };
+    _.extend(Geo.Curve.prototype,{
+        getLength: function(){
+            // corresponds to s1240: void s1240(*curve, double precision, *result length, *result stat(>0= warning, 0=ok, <0=error))
+        },
+        getPointer: function(){
+            return this._pointer;
+        },
+        destroy: function(){
+            Module._free(this._pointer);
+        }
+    });
 
     return Geo;
 });
