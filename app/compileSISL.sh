0b9b7828f45ef131ae01f438be0d51ec1bcb2faf
Phil <phil@phil-seaton.com>
2014-12-29 07:29:07 -0800

    Broke up the compileSISL bash script into many lines so it's readable. The list of accessible functions is now one-per-line. One day, this list could even end up exported into a separate file and read in by the bash script, but for now at least it's not just one endless line.

--- app/compileSISL.sh
+++ app/compileSISL.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+echo ""
 echo "Building javascript..."
 
 # source ~/emsdk-portable/emsdk_set_env.sh
-/home/phil/emscripten/emcc src/SISL/src/*.c -o src/SISL/compiled.js -s EXPORTED_FUNCTIONS="['_newCurve','_freeCurve','_curveParametricEnd','_curveParametricStart','_s1240','_s1227','_s1303']" -s TOTAL_MEMORY=536870912 -O3 --memory-init-file 0
 
+# Run the compile operation!
+/home/phil/emscripten/emcc \
+    src/SISL/src/*.c \
+    -o src/SISL/compiled.js \
+    -s EXPORTED_FUNCTIONS="[
+        '_newCurve',
+        '_freeCurve',
+        '_curveParametricEnd',
+        '_curveParametricStart',
+        '_s1240',
+        '_s1227',
+        '_s1303'
+    ]" \
+    -s TOTAL_MEMORY=536870912 \
+    -O3 \
+    --memory-init-file 0
+
+echo "Done."
+echo ""
