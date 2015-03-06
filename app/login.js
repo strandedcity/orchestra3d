7058961f0bd7797da166f8ea64080f9510855540
Phil Seaton <phil@phil-seaton.com>
2015-03-05 22:00:23 -0800

    Removed whitespace to test git hook

--- app/login.js
+++ app/login.js
@@ -33,7 +33,7 @@ require(["appconfig"],function() {
                     var el = $('#errorMessage');
                     el.text(err.message);
                     el.fadeTo('fast',1);
-
+                    
                 }
 
                 $('#loginbutton').on('click',function(e){
