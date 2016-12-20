34abf185db59ac4282fe8a0ff4fac7e0c6aa4a54
strandedCity <phil@phil-seaton.com>
2016-12-20 09:42:33 -0800

    Change to accomodate a current design I'm working on. This is still a bug.

--- app/src/dataFlow/components/curve.js
+++ app/src/dataFlow/components/curve.js
@@ -262,7 +262,7 @@ define([
             var result = DataMatcher([this.getInput("C"),this.getInput("D"),this.getInput("P")],function(curve, distance, plane){
                 //var normalVect = plane.getNormal();
                 console.warn("NEED TO DEFINE PLANE OBJECT WITH .getNormal() method");
-                return new Geometry.CurveOffset(curve,distance,plane);
+                return new Geometry.CurveOffset(curve,distance,new THREE.Vector3(0,1,0));
             });
 
             this.getOutput("C").replaceData(result.tree);
