82a31f31fd6353e24d880d609046c5378b248f18
strandedCity <phil@phil-seaton.com>
2016-12-21 20:15:50 -0800

    Add s1957, Closest point between a curve and a point

--- app/compileSISL.sh
+++ app/compileSISL.sh
@@ -27,6 +27,7 @@ emcc \
         '_s1360', # Create an approximation of the offset to a curve within a tolerance
         '_s1857', # Find all the intersections between two curves
         '_s1613', # Approximate a curve with a sequence of straight lines
+        '_s1957', # Find the closest point between a curve and a point. Simple version.
     ]" \
     -s TOTAL_MEMORY=536870912 \
     -O3 \
